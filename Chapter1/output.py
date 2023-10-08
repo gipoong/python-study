fc993e4893f07e7edb0ca5943dcb7da4304a26b9
권기풍 <gipoong@naver.com>
2023-10-09 02:30:00 +0900

    output.py commit

---
+++ Chapter1/output.py
@@ -0,0 +1,21 @@
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("# 하나만 출력합니다.")
+    print("Hello Python Programming...!")
+    print()
+
+    print("# 여러 개를 출력합니다.")
+    print(10, 20, 30, 40, 50)
+    print("안녕하세요", "저의", "이름은", "권기풍입니다!")
+    print()
+
+    print("# 아무것도 출력하지 않습니다.")
+    print("---확인 전용선---")
+    print()
+    print()
+    print("---확인 전용선---")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
